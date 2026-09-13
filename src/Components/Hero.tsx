73a922c8f7ad149bf3531fcf635d27becdd84ebc
premrahi <premrahi234@gmail.com>
2026-09-13 22:36:07 +0530

    hero section

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { SplitText } from "gsap/all";
+import { getOptimisedAppearId } from "motion";
 import React from "react";
 
 const Hero: React.FC = () => {
@@ -19,21 +20,24 @@ const Hero: React.FC = () => {
 
         gsap.from(split.words , {opacity:0 , yPercent:100 ,  stagger : 0.1 , duration : 1 , ease:'power1.inOut'})
         gsap.from('#hero p' , {opacity:0 , xPercent:-10 ,   duration : 1 , ease:'power1.inOut'})
+
+        gsap.fromTo('#hero h3' ,{opacity:0 , xPercent:-40} ,{opacity:1 ,duration:1, ease:'power1.inOut' , xPercent:0} )
+
     } ,[]) ;
 
   return (
     <div
-      className="bg-cover bg-center "
+      className="bg-cover bg-center h-[80%]"
       style={{
         backgroundImage:
           "linear-gradient(to left, transparent, rgba(0,0,0,0.8)),url('/images/hero-bg.jpg')",
       }}
     >
-      <section id="hero" className="mx-14">
-        <p className="font-HeptaSlab text-white text-gradient">
+      <section id="hero" className=" mx-14">
+        <p className="font-HeptaSlab  text-white text-gradient">
           Premium Aluminium Composite Panels
         </p>
-        <h1 className="center text-white text-gradient" id="title">
+        <h1 className="center text-white mt-14 mb-28 text-gradient" id="title">
           Surfaces that Define <br />
           <span className=" ">Architecture</span>
         </h1>
